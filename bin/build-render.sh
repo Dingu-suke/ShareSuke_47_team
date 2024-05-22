31d56edf79efb4f17567c90cdd75e864dd99885b
Dingu-suke <e2310000rubik@gmail.com>
2024-05-22 17:13:30 +0900

    [🔧][add .sh]: build-render.sh を作成

---
+++ bin/build-render.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+bundle install
+yarn install
+yarn build # jsファイルをesbuildでバンドルしている
+yarn build:css
+bundle exec ridgepole -c config/database.yml -E production --apply -f db/schemas/Schemafile
+# ridgepole ツールを使用して、Schemafile に記述されたデータベーススキーマ定義をデータベースに適用
+# -c config/database.yml はデータベースの設定ファイルを指定し、-E production はプロダクション環境を対象とすることを明示 => DBスキーマが最新の状態に保、デプロイ時にスキーマの変更が反映される
