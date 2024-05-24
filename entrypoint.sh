c86f3d2004d8729bd9635353fd9f3515da15304f
Dingu-suke <e2310000rubik@gmail.com>
2024-05-24 23:23:54 +0900

    [⭐][Dockefile:試]3

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,17 @@ set -e
 # Rails に対応したファイル server.pid が存在しているかもしれないので削除する。
 rm -f /app/tmp/pids/server.pid
 
-yarn build
-yarn build:css
+NODE_ENV=production yarn build
+CSS_ENV=production yarn build:css
 
 RAILS_ENV=production bundle exec rails assets:clobber
 RAILS_ENV=production bundle exec rails assets:precompile
 RAILS_ENV=production bundle exec rails assets:clean
 RAILS_ENV=production bundle exec rails db:migrate
 
+bundle exec ridgepole -c config/database.yml -E production --apply -f db/schemas/Schemafile
+
 # コンテナーのプロセスを実行する。（Dockerfile 内の CMD に設定されているもの。）
 exec "$@"
+
+echo "entrypoint.sh DONE DONE DONE DONE DONE DONE !!!! "
